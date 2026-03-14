ced587ec6b1e7913184a6270d97f2335977e4b97
kING77-77 <arzumanyanmarat9@gmail.com>
2026-03-14 22:17:59 +0400

    Описание изменений

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { Info } from './info/info';
 import { Rsvp } from './rsvp/rsvp';
 import { Footer } from './footer/footer';
 import { Countdown } from './countdown/countdown';
+import { CouplePhoto } from './couple-photo/couple-photo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Hero, Info, Countdown, Rsvp, Footer],
+  imports: [Hero, Info, Countdown, CouplePhoto, Rsvp, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/couple-photo/couple-photo.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-couple-photo',
+  standalone: true,
+  imports: [],
+  templateUrl: './couple-photo.html',
+  styleUrl: './couple-photo.css'
+})
+export class CouplePhoto {}
